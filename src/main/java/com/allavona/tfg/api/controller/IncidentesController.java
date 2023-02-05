49c345676b0648c7eef247e63554bffa22034db9
Arturo Llavona Vallina <arturo.llavona@gmail.com>
2023-02-05 14:26:05 +0100

    Se añade log4j y se capturan las excepciones

--- src/main/java/com/allavona/tfg/api/controller/IncidentesController.java
+++ src/main/java/com/allavona/tfg/api/controller/IncidentesController.java
@@ -13,11 +13,14 @@ import com.allavona.tfg.business.service.IncidentesService;
 import com.allavona.tfg.business.service.RecursosService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,6 +39,9 @@ public class IncidentesController extends BaseController implements IncidentesAP
     private IncidenteDtoConverter incidenteDtoConverter = new IncidenteDtoConverter();
     private ClasificacionIncidenteDtoConverter clasificacionIncidenteDtoConverter = new ClasificacionIncidenteDtoConverter();
     private TipoRecursoDtoConverter tipoRecursoDtoConverter = new TipoRecursoDtoConverter();
+
+    private static Logger logger = LogManager.getLogger();
+
     @Override
     @RequestMapping(produces = { MediaType.APPLICATION_JSON_VALUE } , method = RequestMethod.GET)
     public ResponseEntity<List<Incidente>> buscarIncidentes(
@@ -43,20 +49,32 @@ public class IncidentesController extends BaseController implements IncidentesAP
             @RequestHeader(value = ID_USUARIO_HEADER, required = true) final Integer idUsuario,
             @Parameter(description = "Parámetro que indica si el incidente ya ha sido finalizado.", required = false, schema = @Schema(type = "boolean"))
             @RequestParam(value = "closed", required = false, defaultValue = "false") final boolean closed) {
-        Usuario usuario = getUsuarioById(idUsuario);
-        SecurityUtils.checkIsUsuarioConPerfilConsulta(usuario);
 
-        List<IncidenteDTO> source;
-        if (closed) {
-            source = incidentesService.findIncidentesFinalizados();
-        } else {
-            source = incidentesService.findIncidentesEnCurso();
+        ResponseEntity<List<Incidente>> respuesta = ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        try {
+            Usuario usuario = getUsuarioById(idUsuario);
+            SecurityUtils.checkIsUsuarioConPerfilConsulta(usuario);
+
+            List<IncidenteDTO> source;
+            if (closed) {
+                source = incidentesService.findIncidentesFinalizados();
+            } else {
+                source = incidentesService.findIncidentesEnCurso();
+            }
+            respuesta = Optional
+                    .of(SecurityUtils.filtrarObservacionesConDatosMedicos(
+                            source.stream().map(i -> incidenteDtoConverter.convert(i)).toList(), usuario)).map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+            logger.info("Incidentes recuperados correctmente por el usuario " + idUsuario);
+
+        } catch (ResponseStatusException e) {
+            logger.warn("Un usuario ha intentado acceder a un recurso al que no tenía permiso.", e.getMessage());
+        } catch (Exception e) {
+            logger.error("Ha ocurrido un error", e.getMessage());
+        } finally {
+            return respuesta;
         }
-        List<Incidente> incidentes = SecurityUtils.filtrarObservacionesConDatosMedicos(
-                source.stream().map(i -> incidenteDtoConverter.convert(i)).toList(), usuario);
-        return Optional
-                .of(incidentes).map(ResponseEntity::ok)
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
     @Override
     @RequestMapping(path = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE } , method = RequestMethod.GET)
@@ -65,12 +83,22 @@ public class IncidentesController extends BaseController implements IncidentesAP
             @RequestHeader(value = ID_USUARIO_HEADER, required = true) final Integer idUsuario,
             @Parameter(description = "Identificador del incidente.", required = true)
             @PathVariable(name="id", required = true) final Integer idIncidente) {
+        ResponseEntity<Incidente> respuesta = ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        try {
+            Usuario usuario = getUsuarioById(idUsuario);
+            SecurityUtils.checkIsUsuarioConPerfilConsulta(usuario);
 
-        Usuario usuario = getUsuarioById(idUsuario);
-        SecurityUtils.checkIsUsuarioConPerfilConsulta(usuario);
-        Incidente incidente = Optional.of(incidentesService.findIncidenteById(idIncidente)).map(i-> incidenteDtoConverter.convert(i)).get();
-        SecurityUtils.filtrarObservacionesConDatosMedicos(incidente, usuario);
-        return Optional.of(incidente).map(ResponseEntity::ok).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+            Incidente incidente = Optional.of(incidentesService.findIncidenteById(idIncidente)).map(i -> incidenteDtoConverter.convert(i)).get();
+            SecurityUtils.filtrarObservacionesConDatosMedicos(incidente, usuario);
+
+            respuesta = Optional.of(incidente).map(ResponseEntity::ok).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        } catch (ResponseStatusException e) {
+            logger.warn("Un usuario ha intentado acceder a un recurso al que no tenía permiso.", e.getMessage());
+        } catch (Exception e) {
+            logger.error("Ha ocurrido un error", e.getMessage());
+        } finally {
+            return respuesta;
+        }
     }
     @Override
     @RequestMapping(path = "/classifications", produces = {MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.GET)
@@ -79,23 +107,37 @@ public class IncidentesController extends BaseController implements IncidentesAP
                     schema = @Schema(type = "string"))
             @RequestParam(value = "codigo", required = false) final String codigo
     ) {
-        return Optional
-                .of(incidentesService.findClasificacionIncidenteByCodigo(codigo)
-                        .stream()
-                        .map(clasificacionIncidenteDTO -> clasificacionIncidenteDtoConverter.convert(clasificacionIncidenteDTO)).toList())
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        ResponseEntity respuesta = ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        try {
+            respuesta = Optional
+                    .of(incidentesService.findClasificacionIncidenteByCodigo(codigo)
+                            .stream()
+                            .map(clasificacionIncidenteDTO -> clasificacionIncidenteDtoConverter.convert(clasificacionIncidenteDTO)).toList())
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        } catch (Exception e) {
+            logger.error("Ha ocurrido un error", e.getMessage());
+        } finally {
+            return respuesta;
+        }
     }
     @Override
     @RequestMapping(path = "/classifications/{id}/recommended-resources-type-list", produces = {MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.GET)
     public ResponseEntity<List<TipoRecurso>> buscarPlantillaClasificacionIncidente(
             @Parameter(description = "Identificador de la clasificación del incidente.", required = true)
             @PathVariable(name="id", required = true) final Integer id) {
-        return Optional
-                .of(recursosService.listResourcesByIncidentClassification(id)
-                        .stream()
-                        .map(tipoRecursoDTO -> tipoRecursoDtoConverter.convert(tipoRecursoDTO)).toList())
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        ResponseEntity<List<TipoRecurso>> respuesta = ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        try {
+            respuesta = Optional.of(recursosService.listResourcesByIncidentClassification(id)
+                            .stream()
+                            .map(tipoRecursoDTO -> tipoRecursoDtoConverter.convert(tipoRecursoDTO)).toList())
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+        } catch (Exception e) {
+            logger.error("Ha ocurrido un error", e.getMessage());
+        } finally {
+            return respuesta;
+        }
     }
 }
